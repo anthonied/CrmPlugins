b477cd0c3c6cbcf8609d6870aa7457301ebd86ee
Anthonie <anthonied@solutionserver.co.za>
2013-06-18 11:58:01 +0200

    First Commit

---
+++ SolEd/CRM.Plugin.SequenceGenerator/SequenceGenerator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace CRM.Plugin.SequenceGenerator
+{
+    public class SequenceGenerator : IPlugin
+    {
+
+        private static readonly object SyncLock = new object();
+        /// <summary>
+        /// A plugin that auto generates the number for entities configured
+        /// </summary>
+        /// <remarks>
+        /// Register this plug-in on the Create message and pre-event stage.
+        /// </remarks>
+        /// <param name="serviceProvider"></param>
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Obtain the execution context from the service provider.
+            var context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            var tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            if (!context.InputParameters.Contains("Target") || !(context.InputParameters["Target"] is Entity))
+            {
+                tracingService.Trace(
+                       "AutoNumber Pluing: The configuration is invalid. Target does not exist. Please contact adminsitrator with the issue.");
+                throw new InvalidPluginExecutionException(
+                    "AutoNumber Pluing: The configuration is invalid. Target does not existd. Please contact adminsitrator with the issue.");
+            }
+
+            var entity = context.InputParameters["Target"] as Entity;
+            
+            try
+            {
+                if (context.IsInTransaction)
+                {
+                    lock (SyncLock)
+                    {
+                        if (entity != null)
+                        {
+
+                            var setting = SequenceHelper.GetSettings(service, entity.LogicalName);
+
+                            if (setting != null && !entity.Attributes.Contains(setting.PropertyName))
+                            {
+                                var next = setting.CurrentPosition + 1;
+                                var nextAlternateClientNumber = "";
+                                if (setting.Prefix != "" && setting.Postfix != "")
+                                {
+                                    tracingService.Trace("The configuration is invalid. Prefix and Postfix are both provided. Please contact adminsitrator with the issue.");
+                                    throw new InvalidPluginExecutionException("The configuration is invalid. Prefix and Postfix are both provided. Please contact adminsitrator with the issue.");
+                                }
+                                if (setting.Prefix != "")
+                                {
+                                    var numberOfSpacingCharacters = setting.Prefix.Contains("#") ? setting.Prefix.Split('#').Length - 1 : 1;
+                                    var cleanPrefix = setting.Prefix.Replace("#", "");
+                                    nextAlternateClientNumber = cleanPrefix + next.ToString(CultureInfo.InvariantCulture).PadLeft(numberOfSpacingCharacters, '0');
+                                }
+                                entity[setting.PropertyName] = nextAlternateClientNumber;                                
+                                setting.Increment(service, next);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                tracingService.Trace("AutoNumber Plugin Exception: " + ex);
+                throw new InvalidPluginExecutionException(ex.ToString());
+            }
+        }
+    }
+}
---
+++ SolEd/CRM.Plugin.SequenceGenerator/SequenceHelper.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections;
+using System.Reflection;
+
+using Microsoft.Crm.Sdk.Messages;
+
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Crm.Sdk;
+
+
+namespace CRM.Plugin.SequenceGenerator {
+    public class SequenceHelper {
+
+        public const string SchemaName = "ce_counter";//Auto Number Entity Name
+
+        public class Fields
+        {
+            public const string Id = "ce_counterid";
+            public const string EntityName = "ce_name";
+            public const string PropertyName = "new_propertyname";
+            public const string CurrentPosition = "ce_nextnumber";
+            public const string Prefix = "ce_prefix";
+            public const string Postfix = "ce_postfix";
+        }
+
+        public Guid Id { get; set; }
+        public string EntityName { get; set; }
+        public string PropertyName { get; set; }
+        public int CurrentPosition { get; set; }
+        public string Prefix { get; set; }
+        public string Postfix { get; set; }
+
+        public SequenceHelper() { }
+
+        public SequenceHelper(Entity entity)
+        {
+            this.Id = entity.Id;
+            this.EntityName = entity[Fields.EntityName].ToString();
+            this.PropertyName = entity[Fields.PropertyName].ToString();
+            this.CurrentPosition = int.Parse(entity[Fields.CurrentPosition].ToString());
+            
+            if (entity.Contains(Fields.Prefix))
+            {
+                this.Prefix = entity[Fields.Prefix].ToString();
+            }
+            else {
+                this.Prefix = "";
+            }
+
+            if (entity.Contains(Fields.Postfix))
+            {
+                this.Postfix = entity[Fields.Postfix].ToString();
+            }
+            else
+            {
+                this.Postfix = "";
+            }
+        }
+
+        public void Increment(IOrganizationService service, int next)
+        {
+            this.CurrentPosition = next; // set before calling ToDynamic
+
+            Entity entity = new Entity(SequenceHelper.SchemaName);
+            
+            entity[Fields.Id] = this.Id;
+            entity[Fields.CurrentPosition] = this.CurrentPosition;
+
+            service.Update(entity);
+        }
+
+        // see if there are any increment settings for this entity
+        public static SequenceHelper GetSettings(IOrganizationService service, string entityName)
+        {
+            SequenceHelper setting = null;
+
+            QueryByAttribute query = new QueryByAttribute();
+            query.Attributes.AddRange(new string[] { SequenceHelper.Fields.EntityName });
+            query.ColumnSet = new ColumnSet(new string[] { SequenceHelper.Fields.Id, SequenceHelper.Fields.EntityName, SequenceHelper.Fields.PropertyName, SequenceHelper.Fields.CurrentPosition, SequenceHelper.Fields.Prefix, SequenceHelper.Fields.Postfix });
+            query.EntityName = SequenceHelper.SchemaName;
+            query.Values.AddRange(new object[] { entityName });
+
+            //Execute using a request to test the OOB (XRM) message contracts
+            RetrieveMultipleRequest request = new RetrieveMultipleRequest();
+            request.Query = query;
+            Collection<Entity> entityList = ((RetrieveMultipleResponse)service.Execute(request)).EntityCollection.Entities;
+
+            if (entityList.Count > 0) // no error checkig to see if there are 2 incrementors set for the same entity
+            {
+                setting = new SequenceHelper(entityList[0]);
+            }
+
+            return setting;
+        }
+
+    }
+}
